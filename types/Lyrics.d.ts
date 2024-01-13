c49ee47bf02fb6c1a01154eaf3a200254b2f4087
Gelianthus <gelotandoc04@gmail.com>
2024-01-13 17:18:52 +0800

    changes to package.json

---
+++ types/Lyrics.d.ts
@@ -0,0 +1,4 @@
+declare module './Lyrics' {
+  const Lyrics: any /* type definition for your Lyrics component */;
+  export default Lyrics;
+}
---
+++ types/entrypoint.d.ts
@@ -0,0 +1,3 @@
+// types/index.d.ts
+/// <reference path="./Lyrics.d.ts" />
+/// <reference path="./index.d.ts" />
