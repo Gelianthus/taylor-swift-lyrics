6c1e08c963eb9d578beb848be97a93bc69850048
Gelianthus <gelotandoc04@gmail.com>
2024-01-19 04:43:28 +0800

    added a default value for customStyles prop

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import Lyrics from "./Lyrics";
 import React from "react";
 
-function TaylorSwiftLyrics({ song, customStyles }) {
-	const { wrapper, title, line_container, lines } = customStyles;
+function TaylorSwiftLyrics({ song, customStyles = {} }) {
+	const {
+		wrapper = "p-8",
+		title = "text-2xl mb-8 font-semibold",
+		line_container = "space-y-4",
+		lines = "",
+	} = customStyles;
 
 	return (
 		<Lyrics
 			song={song}
 			customStylesObj={{
-				wrapper: wrapper || "p-8",
-				title: title || "text-2xl mb-8 font-semibold",
-				line_container: line_container || "space-y-4",
-				lines: lines || "",
+				wrapper,
+				title,
+				line_container,
+				lines,
 			}}
 		/>
 	);
