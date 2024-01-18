eb84c4b5fc5e123cdfcc2886c29b0bea0c9cc553
Gelianthus <gelotandoc04@gmail.com>
2024-01-19 04:25:18 +0800

    fixed the issue with styling

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import Lyrics from "./Lyrics";
 import React from "react";
 
 function TaylorSwiftLyrics({ song, customStyles }) {
+	const { wrapper, title, line_container, lines } = customStyles;
+
 	return (
 		<Lyrics
 			song={song}
-			customStylesObj={
-				customStyles || {
-					wrapper: "p-8",
-					title: "text-2xl mb-8 font-semibold",
-					line_container: "space-y-4",
-					lines: "",
-				}
-			}
+			customStylesObj={{
+				wrapper: wrapper || "p-8",
+				title: title || "text-2xl mb-8 font-semibold",
+				line_container: line_container || "space-y-4",
+				lines: lines || "",
+			}}
 		/>
 	);
 }
